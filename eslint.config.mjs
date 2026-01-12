5c535673535e4389cb598805d27be67e429dd4f2
Yuchen Weng <10099565@mcpsmd.net>
2026-01-12 16:28:48 +0000

    更新 ESLint 配置，添加语言选项以支持 JSX 语法

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,14 @@ export default [
     js.configs.recommended,
     {
         files: ["**/*.js", "**/*.jsx"],
-        
+        languageOptions: {
+            globals: { ...globals.browser, ...globals.node },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                }
+            }
+        }
     },
     prettier
 ]
