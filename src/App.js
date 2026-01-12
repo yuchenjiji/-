3ffa6b533476d5f6358b641c314d1ebeec30a35b
Yuchen Weng <10099565@mcpsmd.net>
2026-01-12 01:42:23 +0000

    更新 Pizza 组件以接受 props，支持动态显示名称和描述

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const Pizza = () => {
+const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("p", {}, props.description),
