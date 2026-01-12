0a74b1376a1ec282bc87a9149f5eea265b5e233c
Yuchen Weng <10099565@mcpsmd.net>
2026-01-12 01:39:33 +0000

    在 App 组件中添加多个 Pizza 组件实例，更新为显示不同种类的披萨及其描述

--- src/App.js
+++ src/App.js
@@ -16,10 +16,21 @@ const App = () => {
         name: "Margherita",
         description: "Classic pizza with tomato sauce, mozzarella, and basil",
       }),
-      React.createElement(Pizza),
-      React.createElement(Pizza),
-      React.createElement(Pizza),
-      React.createElement(Pizza),
+      React.createElement(Pizza, {
+        name: "Pepperoni",
+        description: "Spicy pepperoni with mozzarella and tomato sauce",}),
+      React.createElement(Pizza, {
+        name: "Vegetarian",
+        description: "A mix of seasonal vegetables with mozzarella and tomato sauce",
+      }),
+      React.createElement(Pizza, {
+        name: "BBQ Chicken",
+        description: "Grilled chicken with BBQ sauce, red onions, and cilantro",
+      }),
+      React.createElement(Pizza, {
+        name: "Hawaiian",
+        description: "Ham and pineapple with mozzarella and tomato sauce",
+      }),
     ]
   );
 };
