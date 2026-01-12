edeba0f3db83d66d43d0b8a68f46cea58c58475c
Yuchen Weng <10099565@mcpsmd.net>
2026-01-12 21:01:01 +0000

    feat: 重构 Pizza 组件，使用 JSX 语法替代 React.createElement

--- Pizza.jsx
+++ Pizza.jsx
@@ -1,10 +1,3 @@
-// const Pizza = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("p", {}, props.description),
-//   ]);
-// };
-
 const Pizza = (props) => {
     return (
         <div className="pizza">
