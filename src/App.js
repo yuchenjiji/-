521101239c91105ca46581d56224a4028596ae7d
Yuchen Weng <10099565@mcpsmd.net>
2026-01-12 02:08:45 +0000

    优化 App 组件的代码格式，提升可读性

--- src/App.js
+++ src/App.js
@@ -3,36 +3,33 @@ const Pizza = (props) => {
     React.createElement("h1", {}, props.name),
     React.createElement("p", {}, props.description),
   ]);
-
-}
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Padre Gino's"),
-      React.createElement(Pizza, {
-        name: "Margherita",
-        description: "Classic pizza with tomato sauce, mozzarella, and basil",
-      }),
-      React.createElement(Pizza, {
-        name: "Pepperoni",
-        description: "Spicy pepperoni with mozzarella and tomato sauce",}),
-      React.createElement(Pizza, {
-        name: "Vegetarian",
-        description: "A mix of seasonal vegetables with mozzarella and tomato sauce",
-      }),
-      React.createElement(Pizza, {
-        name: "BBQ Chicken",
-        description: "Grilled chicken with BBQ sauce, red onions, and cilantro",
-      }),
-      React.createElement(Pizza, {
-        name: "Hawaiian",
-        description: "Ham and pineapple with mozzarella and tomato sauce",
-      }),
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "Margherita",
+      description: "Classic pizza with tomato sauce, mozzarella, and basil",
+    }),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "Spicy pepperoni with mozzarella and tomato sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "Vegetarian",
+      description:
+        "A mix of seasonal vegetables with mozzarella and tomato sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "BBQ Chicken",
+      description: "Grilled chicken with BBQ sauce, red onions, and cilantro",
+    }),
+    React.createElement(Pizza, {
+      name: "Hawaiian",
+      description: "Ham and pineapple with mozzarella and tomato sauce",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
