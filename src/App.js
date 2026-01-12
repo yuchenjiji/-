e34b219413d41b20ce5673aa723cbf412e482d3d
Yuchen Weng <10099565@mcpsmd.net>
2026-01-12 18:13:16 +0000

    feat: 更新 App.js，添加 React 和 ReactDOM 的导入，使用 createRoot 渲染组件

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -33,5 +36,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
