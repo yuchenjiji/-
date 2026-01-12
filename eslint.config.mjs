072d91b042174cdbc3ec46e0e80b746cb7f9bf96
Yuchen Weng <10099565@mcpsmd.net>
2026-01-12 13:27:51 +0000

    添加 ESLint 配置文件并初始化 Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.js", "**/*.jsx"],
+        
+    },
+    prettier
+]
